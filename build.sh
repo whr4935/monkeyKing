a3a0066a18132a2fa58f2cb5a0d6def30f4bf658
houren wang <wanghourenrd@163.com>
2020-01-30 22:58:00 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -x
 
 SOURCE_DIR=`pwd`/
-BUILD_DIR=${BUILD_DIR:-./out}
-INSTALL_DIR=${INSTALL_DIR:-${BUILD_DIR}/install}
+BUILD_OUT_DIR=${BUILD_OUT_DIR:-./out}
+INSTALL_DIR=${INSTALL_DIR:-${BUILD_OUT_DIR}/install}
 
-ln -sf $BUILD_DIR/compile_commands.json
+ln -sf $BUILD_OUT_DIR/compile_commands.json -t . -r
 
-mkdir -p $BUILD_DIR \
-  && cd $BUILD_DIR \
+mkdir -p $BUILD_OUT_DIR \
+  && cd $BUILD_OUT_DIR \
   && cmake \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
@@ -17,7 +17,7 @@ mkdir -p $BUILD_DIR \
   && make $*
 
 # Use the following command to run all the unit tests
-# at the dir $BUILD_DIR/$BUILD_TYPE :
+# at the dir $BUILD_OUT_DIR/$BUILD_TYPE :
 # CTEST_OUTPUT_ON_FAILURE=TRUE make test
 
 # cd $SOURCE_DIR && doxygen
